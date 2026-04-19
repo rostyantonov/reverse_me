1da0fad28a0deae1f9996a558266be6c9b22fa8e
Rostyslav Antonov <rostyslav_antonov@epam.com>
2026-04-19 12:33:32 +0300

    Starting the project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.20"
+}
+
+group = "com.over"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
